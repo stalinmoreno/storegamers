96b2b50771559e1ab447d6be46016589094b46f5
JeanPalacios <sheanpalacios@gmail.com>
2021-05-13 16:06:24 -0500

    modulo Orden

--- src/main/java/com/storegamers/appweb/model/Orden.java
+++ src/main/java/com/storegamers/appweb/model/Orden.java
@@ -1,5 +1,35 @@
 package com.storegamers.appweb.model;
 
+import java.math.BigDecimal;
+
 public class Orden {
+    private long id;
+    private String descripcion;
+    private BigDecimal precio;
+
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return this.descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public BigDecimal getPrecio() {
+        return this.precio;
+    }
+
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
 
 }
